c51a5b5ba97880cbe4c7e30b4c99dd89dad9f930
Scott Lobdell <slobdell@hearsaycorp.com>
2015-01-24 11:00:50 -0800

    try redirect again

--- workout_generator/basic_navigation/utils.py
+++ workout_generator/basic_navigation/utils.py
@@ -1,21 +1,18 @@
-import os
-import re
-
-from django.http import HttpResponsePermanentRedirect
+from django.conf import settings
+from django.http import HttpResponseRedirect
 
 
 def requires_ssl(fn):
     def inner(request, *args, **kwargs):
-        if os.environ.get("I_AM_IN_DEV_ENV"):
-            return fn(request, *args, **kwargs)
-
-        if request.is_secure() or 'HTTP_X_SSL_PROTOCOL' in request.META:
-            return fn(request, *args, **kwargs)
-        secure_url = make_https(request.build_absolute_uri())
-        return HttpResponsePermanentRedirect(secure_url)
+        if not any([
+            settings.DEBUG,
+            request.is_secure(),
+            request.META.get("HTTP_X_FORWARDED_PROTO", "") == 'https',
+            'HTTP_X_SSL_PROTOCOL' in request.META
+        ]):
+            url = request.build_absolute_uri(request.get_full_path())
+            secure_url = url.replace("http://", "https://")
+            return HttpResponseRedirect(secure_url)
+        return fn(request, *args, **kwargs)
 
     return inner
-
-
-def make_https(url):
-    return re.sub('^\s*https?:', 'https:', url)
