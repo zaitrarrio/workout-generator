4efb02e222abe65e489e388a8dee97342fe8e765
Scott Lobdell <slobdell@hearsaycorp.com>
2015-01-24 09:04:01 -0800

    dont use sslify

--- workout_generator/settings.py
+++ workout_generator/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'sslify.middleware.SSLifyMiddleware',
+    # 'sslify.middleware.SSLifyMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
