5fb34f937097efda4cd9f4c9ec8cae05a3b7bfe2
Scott Lobdell <slobdell@hearsaycorp.com>
2015-01-29 13:56:10 -0700

    meh, coupon code change

--- workout_generator/scripts/create_coupon.py
+++ workout_generator/scripts/create_coupon.py
@@ -3,10 +3,14 @@ import datetime
 from workout_generator.coupon.models import Coupon
 
 Coupon.create("GRANDFATHER500",
-              datetime.datetime(year=2015, month=2, day=31, hour=0, minute=0),
-              "Hey, alright!  Your membership is free, just finished the signup!",
+              datetime.datetime(year=2015, month=3, day=1, hour=0, minute=0),
+              "Hey, alright!  Your membership is free, just finish the signup!",
               None)
 Coupon.create("HACKERNEWS2015",
               datetime.datetime(year=2015, month=2, day=7, hour=0, minute=0),
-              "Hey, alright!  Your membership is free, just finished the signup!",
+              "Hey, I read Hacker News too!  Your membership is free, just finish the signup!",
+              None)
+Coupon.create("FACEBOOK_DJLTEVMKGDDVJKLF",
+              datetime.datetime(year=2015, month=2, day=14, hour=0, minute=0),
+              "Hey, alright!  Your membership is free, just finish the signup!",
               None)
