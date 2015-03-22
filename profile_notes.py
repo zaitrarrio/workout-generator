15dd1f38585741dc38c05b2af627bf72a94c6cfb
Scott Lobdell <slobdell@hearsaycorp.com>
2015-03-21 21:21:06 -0700

    profiling

--- profile_notes.py
+++ profile_notes.py
@@ -1,9 +1,8 @@
 # pip install pycallgraph
+import django
+django.setup()
 
-from pycallgraph import PyCallGraph
-from pycallgraph.output import GraphvizOutput
 from workout_generator.user.models import User
 from workout_generator.workout.generator import generate_new_workouts
 user = User.get_by_username("scott.lobdell@gmail.com")
-with PyCallGraph(output=GraphvizOutput()):
-    generate_new_workouts(user)
+generate_new_workouts(user)
